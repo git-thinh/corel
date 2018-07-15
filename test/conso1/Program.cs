ff7cf1cd6d62e31ecbbb28e549133de3bd2cc667
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-07-15 13:23:06 +0700

    a

--- test/conso1/Program.cs
+++ test/conso1/Program.cs
@@ -1,4 +1,4 @@
-﻿using CSharpTest.Net.IpcChannel;
+﻿using IpcChannel;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
--- test/conso1/lib/IpcChannel/IpcChannelRegistrar.cs
+++ test/conso1/lib/IpcChannel/IpcChannelRegistrar.cs
@@ -1,25 +1,9 @@
-﻿#region Copyright 2010-2012 by Roger Knapp, Licensed under the Apache License, Version 2.0
-/* Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-#endregion
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using CSharpTest.Net.Synchronization;
-using CSharpTest.Net.Threading;
 using Microsoft.Win32;
 
-namespace CSharpTest.Net.IpcChannel
+namespace IpcChannel
 {
     /// <summary>
     /// Provides a default implementation of the channel registrar usuing the system registry as the 
